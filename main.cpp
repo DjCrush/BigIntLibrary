c960e91248d3c0e506a718b733246c46dbccdc80
Yevgeny Antonov <yevgeny1978@mail.ru>
2019-02-22 12:53:18 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,26 +4,19 @@
 using namespace std;
 
 BigInt fact(int N);
-//BigInt fibonacci(int N);
 
 int main()
 {
-	BigInt a;
-	a = 3;
-	int i = 3;
-	a = a * i;
-	cout << a;
-	//cout << fact(12) << endl;
+	cout << fact(1000) << endl;
 	return 0;
 }
-
 
 BigInt fact(int N)
 {
 	BigInt a = 1;
 	if (N != 0)
 	{
-		for (int i = 1; i <= N; i++)
+		for (int i = 2; i <= N; i++)
 		{
 			a = a * i;
 			cout << a << endl;
@@ -31,26 +24,3 @@ BigInt fact(int N)
 	}
 	return a;
 }
-/*
-
-BigInt fibonacci(string N)
-{
-	int index = 0;
-	BigInt a, b, c;
-	do {
-		if (index == 0)
-		{
-			a = "0"; b = "0";
-		}
-		if (index == 1)
-		{
-			a = "1";
-		}
-		c = a + b;
-		a = b;
-		b = c;
-		++index;
-	} while (c != N);
-	return c;
-}
-*/
