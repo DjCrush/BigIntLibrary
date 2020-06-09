f6e916ca1d9c9956922d8a22744d4a4220b9921d
Yevgeny Antonov <yevgeny1978@mail.ru>
2020-06-09 14:36:48 +0700

    Update main.cpp
    Refactoring

--- main.cpp
+++ main.cpp
@@ -1,25 +1,24 @@
 // long numbers v 1.3
 #include <iostream>
+#include <thread>
 #include "bigint.h"
-
+#include <chrono>
 
 BigInt fact(int N)
 {
 	BigInt a = 1;
-	if (N != 0)
+	for (int i = 1; i <= N; i++)
 	{
-		for (int i = 2; i <= N; i++)
-		{
-			a = a * i;
-			std::cout << a << std::endl;
-		}
+		a *= i;
 	}
 	return a;
 }
 
-
 int main()
 {
+	std::chrono::high_resolution_clock::time_point time_begin = std::chrono::high_resolution_clock::now();
 	std::cout << fact(1000) << std::endl;
+	std::chrono::high_resolution_clock::time_point time_end = std::chrono::high_resolution_clock::now();
+	std::cout << std::chrono::duration_cast<std::chrono::microseconds>(time_end - time_begin).count();
 	return 0;
 }
