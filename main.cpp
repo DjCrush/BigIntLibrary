017d2c07128699fe94d1219f725afead95f9ef77
Yevgeny Antonov <yevgeny1978@mail.ru>
2022-08-01 23:14:28 +0700

    Update main.cpp
    Added full-fledged work with negative numbers. The code has been improved.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
-// long numbers v 1.3
 #include <iostream>
-#include <chrono>
-#include "bigint.h"
+#include "BigInteger.h"
 
 BigInteger fact(int N)
 {
@@ -13,21 +11,37 @@ BigInteger fact(int N)
 	return a;
 }
 
+BigInteger fibonacci(int n)
+{
+	BigInteger a("0");
+	BigInteger b("1");
+	BigInteger c("0");
+	for (int i = 1; i < n; ++i)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	return c;
+}
+
 int main()
 {
-    std::cout << "20! = " << fact(20) << std::endl;
-    std::cout << "30! = " << fact(30) << std::endl;
-    std::cout << "1000! = " << fact(1000) << std::endl;
-    for(BigInteger i = -1000; i <= 1000; i++)
-    {
-        std::cout << i << "   ";
-    }
-    std::cout << '\n';
-    BigInteger i = 1000;
-    for(; i >= 1; i--)
-    {
-        std::cout << i << ' ';
-    }
-    std::cout << i << '\n';
-	return 0;
+	std::cout << "Fibonacci 100000 = " << fibonacci(100000) << std::endl << std::endl;
+    	std::cout << "20! = " << fact(20) << std::endl;
+   	std::cout << "30! = " << fact(30) << std::endl;
+    	std::cout << "1000! = " << fact(1000) << std::endl;
+	std::cout << "100000! = " << fact(100000) << std::endl;
+    	for(BigInteger i = -1000; i <= 1000; i++)
+    	{
+		std::cout << i << "   ";
+    	}
+    	std::cout << std::endl;
+    	BigInteger i = 1000;
+    	for(; i >= 1; i--)
+    	{	
+        	std::cout << i << ' ';
+    	}
+    	std::cout << i << std::endl;
+    	return 0;
 }
